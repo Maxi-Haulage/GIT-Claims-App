190cbdfe610e5b5fd065df32f911b0cd1e0d90cc
phantomsheepp <rachel.horton1@icloud.com>
2024-08-12 17:05:10 +0100

    added updates and police information

--- claims_app/claims/admin.py
+++ claims_app/claims/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from claims.models import Claim
+from claims.models import Claim, Update, Police
 
 class ClaimAdmin(admin.ModelAdmin):
-    list_display = ("reference", "date", "cost", "incident_type", "description")
+    list_display = ("id", "incident_date", "claim_date", "cost", "incident_type", "company", "description")
+
+class UpdateAdmin(admin.ModelAdmin):
+    list_display = ("id", "claim", "date", "note")
+
+class PoliceAdmin(admin.ModelAdmin):
+    list_display = ("claim", "force", "note")
 
 admin.site.register(Claim, ClaimAdmin)
+admin.site.register(Update, UpdateAdmin)
+admin.site.register(Police, PoliceAdmin)
--- claims_app/claims/models.py
+++ claims_app/claims/models.py
@@ -13,9 +13,54 @@ class Claim(models.Model):
         "OTHER" : "Other"
     }
 
-    date = models.DateField()
-    cost = models.DecimalField(decimal_places=2, max_digits=10)
+    DEPOTS = {
+        "CAPEW" : "Cape, Warwick",
+        "WEDGN" : "Wedgenock, Warwick",
+        "HEATH" : "Heathcote, Warwick",
+        "WARRI" : "Warrington",
+        "BELSH" : "Bellshill",
+        "IMMIN" : "Immingham"
+    }
+
+    # Dates
+    incident_date = models.DateField()
+    claim_date = models.DateField()
+
+    # 
+    cost = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    weight = models.FloatField(blank=True, null=True)
     incident_type = models.CharField(choices=INCIDENT_TYPES, max_length=5)
+    
+    # Companies involved
+    company = models.CharField(max_length=100)
+    secondary = models.CharField(max_length=100, blank=True)
+
+    # Reference numbers
+    ajg_ref = models.CharField(max_length=50, blank=True)
+    maxi_ref = models.CharField(max_length=50, blank=True)
+    company_ref = models.CharField(max_length=50, blank=True)
+    
+    
     description = models.CharField(blank=True, max_length=500)
+
+    # Extra information
+    driver = models.CharField(blank=True, max_length=100)
+    location = models.CharField(blank=True, max_length=100)
+    depot = models.CharField(blank=True, choices=DEPOTS, max_length=5)
+    police_involved = models.BooleanField(default=False)
     
 
+class Update(models.Model):
+    claim = models.ForeignKey(Claim, on_delete=models.CASCADE)
+
+    note = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    
+
+class Police(models.Model):
+    claim = models.OneToOneField(Claim, on_delete=models.CASCADE)
+
+    force = models.CharField(max_length=100)
+    officer = models.CharField(max_length=100)
+    reference_no = models.CharField(max_length=50)
+    note = models.CharField(max_length=200, blank=True)
