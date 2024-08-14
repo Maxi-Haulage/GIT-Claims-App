c9ea82b921441a4bbf343f5a4e0c6f7e6230d1fe
phantomsheepp <rachel.horton1@icloud.com>
2024-08-14 11:12:24 +0100

    improved table view

--- frontend/src/components/ViewAll.js
+++ frontend/src/components/ViewAll.js
@@ -6,13 +6,13 @@ export default function ViewAll() {
     const [active, setActive] = useState([]);
     const [closed, setClosed] = useState([]);
 
-    const stats = [active, closed];
+    const stats = {"Active": active, "Closed": closed};
 
     useEffect(() => {
-        axios.get('http://localhost:8000/claims/view-active')
+        axios.get(`http://localhost:8000/claims/view-active`)
         .then(response => {            
             setActive(response.data);
-        }) 
+        })  
         .catch(error => {
             console.log(error);
         });
@@ -29,39 +29,47 @@ export default function ViewAll() {
     }, []);
 
     return (
-        <div>
-            {stats.map((stat) => (
-            <table className="claims-table" key={stat}>
+        <div className='tables'>
+            {Object.keys(stats).map((stat) => (
+            <div key={stat}>
+                <h2>{stat}</h2>
 
-                <thead>
-                <tr>
-                    <th></th>
-                    <th>Company</th>
-                    <th>Date of Incident</th>
-                    <th>Incident Type</th>
-                    <th>Cost</th>
-                    <th>AJG Ref.</th>
-                    <th>Last Update</th>
-                    <th>Status</th>
-                </tr>
-                </thead>
-                
-                <tbody>
-                {stat.map((item) => (
-                <tr key={item.id}>
-                    <th>{item.id}</th>
-                    <th>{item.company}</th>
-                    <th>{item.incident_date}</th>
-                    <th>{item.incident_type}</th>
-                    <th>{item.cost}</th>
-                    <th>{item.ajg_ref}</th>
-                    <th>{item.last_updated}</th>
-                    <th>{item.status}</th>
-                </tr>
-                ))}
-                </tbody>
-                
-            </table>
+                <table className="claims-table" key={stat}>
+
+                    <thead>
+                    <tr>
+                        <th className='reference'></th>
+                        <th>Company</th>
+                        <th>Incident Date</th>
+                        <th>Claim Date</th>
+                        <th>Incident Type</th>
+                        <th>Weight</th>
+                        <th>Cost</th>
+                        <th>AJG Ref.</th>
+                        <th>Last Update</th>
+                        <th className='desc'>Description</th>
+                    </tr>
+                    </thead>
+                    
+                    <tbody>
+                    {stats[stat].map((item) => (
+                    <tr key={item.id}>
+                        <td className='reference'>{item.id}</td>
+                        <td>{item.company}</td>
+                        <td>{item.incident_date}</td>
+                        <td>{item.claim_date}</td>
+                        <td>{item.incident_type}</td>
+                        <td>{item.weight}</td>
+                        <td>£{item.cost}</td>
+                        <td>{item.ajg_ref}</td>
+                        <td>{item.last_updated}</td>
+                        <td className='desc'>{item.description}</td>
+                    </tr>
+                    ))}
+                    </tbody>
+                    
+                </table>
+            </div>
             ))}
         </div>
     );
